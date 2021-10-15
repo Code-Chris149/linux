3904a530faa7078f11c82d504af9ce01d44d46ac
Code-Chris149 <58843157+Code-Chris149@users.noreply.github.com>
2021-10-15 13:22:54 +0200

    Update structure.sh

--- structure.sh
+++ structure.sh
@@ -6,5 +6,24 @@ if [ "" = "$PKG_OK" ]; then
   sudo apt-get --yes install $REQUIRED_PKG
 fi
 cd ~
-mkdir -p test-I-1/$USER/{level1a,level1b/{levelb2a,levelb2b/levelb3a,levelb2c},level1c}
+mkdir -p test-I-1/$USER/{"Tiberian Series"/{"Command & Conquer","Tiberian Sun",Renegade,"Tiberium Wars","Tiberian Twilight"},"Red Alert Series"/{"Red Alert","Red Alert 2","Red Alert 3"},"Generals Series"/Generals}
 tree
+touch ~/test-I-1/$USER/"-obsolete.txt"
+touch ~/test-I-1/$USER/"Red Alert Series"/"Red Alert"/"Command & Conquer: Red Alert – Counterstrike.exe"
+touch ~/test-I-1/$USER/"Red Alert Series"/"Red Alert"/"Command & Conquer: Red Alert – The Aftermath.exe"
+touch ~/test-I-1/$USER/"Red Alert Series"/"Red Alert"/"Command & Conquer: Red Alert – Retaliation.exe"
+touch ~/test-I-1/$USER/"Red Alert Series"/"Red Alert"/"Command & Conquer: Tiberian Sun – Firestorm.exe"
+
+touch ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Tricia Helfer	Kilian Quatar (Nod General)" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Josh Holloway	Ajay (Nod Intelligence Officer)" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Micheal Ironside	Jack Granger (GDI General)" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Joseph D. Duncan	Kane" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Jennifer Morrison	Kirce James (GDI Intelligence Officer)" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Grace Park	Lt. Sandra Telfair" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Billy Dee Williams	Redmond Boyle (GDI Director)" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Shauntay Hinton	Brittany Bhima" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Shanon Cook	Cassandra Blair" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Hohn Huck	William Frank" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+echo "Masha Sapron	Dr. Emel Ibrahiim" >> ~/test-I-1/$USER/"Tiberian Series"/"Tiberium Wars"/cast.txt
+
